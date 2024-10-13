ee0f6a13b5e2c8f63f511dad13e494292eabeb72
S P <suman_pothula@yahoo.com>
2024-10-13 12:31:22 -0700

    Serve front-end app from backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os
 
 
 app = Flask(__name__)
@@ -11,6 +11,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///applications.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+frontend_folder = os.path.join(os.getcwd(),"..", "frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+# serve static files from "dist" folder under the "frontend" directory
+@app.route("/", defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+     if not filename:
+          filename="index.html"
+     return send_from_directory(dist_folder, filename)
+
+# api routes
 import routes
 with app.app_context():
      db.create_all()
